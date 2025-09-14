4c6e4e9fc873cef3a501a344e9ac3bac769fa5e3
y21dawya <y21dawya@latymer.co.uk>
2025-09-14 16:46:57 +0100

    [message]

--- main.py
+++ main.py
@@ -8,32 +8,52 @@ stops = pd.read_csv("bus-stops.csv")
 
 stop_ID = ""
 
-async def sms_to_naptan(sms):
+def sms_to_naptan(sms):
     global stop_ID
     for _, stop in stops.iterrows():
         if str(stop['Bus_Stop_Code']) == sms:
             stop_ID = stop['Naptan_Atco']
 
-async def stop_code_to_naptan(code):
+def stop_code_to_naptan(code):
     global stop_ID
     for _, stop in stops.iterrows():
         if str(stop['Stop_Code_LBSL']) == code:
             stop_ID = stop['Naptan_Atco']
 
-async def stop_search(name):
+def stop_search(name):
     global stop_ID
-    global candidates
-    global final
-    final = []
-    candidates = []
+    global list
+    list = [[],[],[]]
     for _, stop in stops.iterrows():
-        for item in name.lower().split():
-            if item in stop['Stop_Name'].lower().split():
-                candidates.append(stop['Stop_Name'])
-    for item in candidates:
-        if name in item.lower():
-            final.append(item)
-    print(", ".join(final))
+        search = True
+        for word in name.lower().split():
+            if word not in stop['Stop_Name'].lower().split():
+                search = False
+        if search:
+            stop_direction = ""
+            if stop['Heading'] < 23 and stop['Heading'] > 337:
+                stop_direction = " Northbound"
+            elif stop['Heading'] < 68 and stop['Heading'] > 22: 
+                stop_direction = " Northeastbound"
+            elif stop['Heading'] < 113 and stop['Heading'] > 67:
+                stop_direction = " Eastbound"
+            elif stop['Heading'] < 158 and stop['Heading'] > 112:
+                stop_direction = " Southeastbound"
+            elif stop['Heading'] < 203 and stop['Heading'] > 157:
+                stop_direction = " Southbound"
+            elif stop['Heading'] < 248 and stop['Heading'] > 202:
+                stop_direction = " Southwestbound"
+            elif stop['Heading'] < 293 and stop['Heading'] > 247:
+                stop_direction = " Westbound"
+            elif stop['Heading'] < 338 and stop['Heading'] > 292:
+                stop_direction = " Northwestbound"
+            list[0].append(stop['Stop_Name'])
+            list[1].append(stop['Naptan_Atco'])
+            list[2].append(stop_direction)
+    for i in range(len(list[0])):
+        print(f"{i+1}. {list[0][i]}, {list[1][i]}, {list[2][i]}")
+    choice = int(input("Please enter the number of the stop you want to use: \n").strip())
+    stop_ID = list[1][choice-1]
 
 app_id = "Bus_Times_Lite"
 app_key = "f485d0c23eaa46dd8af5841ba61ece70"
